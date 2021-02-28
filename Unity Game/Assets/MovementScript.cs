43f439a62834ed5f5b09fd9c0f070e047b362780
lovettehlers <77397116+lovettehlers@users.noreply.github.com>
2021-02-28 12:50:30 -0500

    Player Movement

---
+++ Unity Game/Assets/MovementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+
+    private Rigidbody2D rb2D;
+    private float thrust = 1000.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("Jump");
+            rb2D.AddForce(transform.up * thrust);
+        }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(horizontalInput, 0, 0) * 10 * Time.deltaTime);
+    }
+}
